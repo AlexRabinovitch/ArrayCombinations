f7b81aa533da6b30e4980821458dda995b5bf669
Alex <alex.rabinovitch.public@gmail.com>
2019-10-27 11:06:38 +0200

    Init

---
+++ ArrayCombinations/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ArrayCombinations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.gbOrder = new System.Windows.Forms.GroupBox();
+            this.rbEfficiency = new System.Windows.Forms.RadioButton();
+            this.rbSum = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMandatory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudMinSum = new System.Windows.Forms.NumericUpDown();
+            this.gbOrder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinSum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 234);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(6, 323);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(1186, 364);
+            this.txtLog.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(126, 22);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(637, 26);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.Text = "62.00,57.50,51.00,49.99,36.00,26.70,23.45,19.50,16.50,13.95,12.50,11.07,10.50,9.9" +
+    "8,9.84,9.00";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1010, 234);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // gbOrder
+            // 
+            this.gbOrder.Controls.Add(this.rbEfficiency);
+            this.gbOrder.Controls.Add(this.rbSum);
+            this.gbOrder.Location = new System.Drawing.Point(27, 191);
+            this.gbOrder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbOrder.Name = "gbOrder";
+            this.gbOrder.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gbOrder.Size = new System.Drawing.Size(273, 89);
+            this.gbOrder.TabIndex = 6;
+            this.gbOrder.TabStop = false;
+            this.gbOrder.Text = "Order By";
+            // 
+            // rbEfficiency
+            // 
+            this.rbEfficiency.AutoSize = true;
+            this.rbEfficiency.Location = new System.Drawing.Point(146, 43);
+            this.rbEfficiency.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbEfficiency.Name = "rbEfficiency";
+            this.rbEfficiency.Size = new System.Drawing.Size(102, 24);
+            this.rbEfficiency.TabIndex = 1;
+            this.rbEfficiency.TabStop = true;
+            this.rbEfficiency.Text = "Efficiency";
+            this.rbEfficiency.UseVisualStyleBackColor = true;
+            // 
+            // rbSum
+            // 
+            this.rbSum.AutoSize = true;
+            this.rbSum.Checked = true;
+            this.rbSum.Location = new System.Drawing.Point(9, 43);
+            this.rbSum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.rbSum.Name = "rbSum";
+            this.rbSum.Size = new System.Drawing.Size(100, 24);
+            this.rbSum.TabIndex = 0;
+            this.rbSum.TabStop = true;
+            this.rbSum.Text = "Max Sum";
+            this.rbSum.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "All numbers:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mandatory numbers:";
+            // 
+            // txtMandatory
+            // 
+            this.txtMandatory.Location = new System.Drawing.Point(184, 62);
+            this.txtMandatory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMandatory.Name = "txtMandatory";
+            this.txtMandatory.Size = new System.Drawing.Size(578, 26);
+            this.txtMandatory.TabIndex = 8;
+            this.txtMandatory.Text = "36.00,16.50,10.50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(794, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Min pack sum:";
+            // 
+            // nudMinSum
+            // 
+            this.nudMinSum.DecimalPlaces = 1;
+            this.nudMinSum.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.nudMinSum.Location = new System.Drawing.Point(915, 18);
+            this.nudMinSum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nudMinSum.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudMinSum.Name = "nudMinSum";
+            this.nudMinSum.Size = new System.Drawing.Size(74, 26);
+            this.nudMinSum.TabIndex = 11;
+            this.nudMinSum.ThousandsSeparator = true;
+            this.nudMinSum.Value = new decimal(new int[] {
+            73,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.nudMinSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMandatory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbOrder);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbOrder.ResumeLayout(false);
+            this.gbOrder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinSum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox gbOrder;
+        private System.Windows.Forms.RadioButton rbEfficiency;
+        private System.Windows.Forms.RadioButton rbSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMandatory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudMinSum;
+    }
+}
+
---
+++ ArrayCombinations/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayCombinations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[] _input = null;
+        List<double> _sums = null;
+        double _minSum = 73;
+        //List<List<Pack>> _packs = null;
+        List<PackSet> _packs = null;
+        StringBuilder _logSB = null;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ClearLog();
+
+            string[] strInput = txtInput.Text.Split(',');
+            _input = Array.ConvertAll(strInput, element => double.Parse(element));
+            _sums = new List<double>();
+
+            string[] strMandatoryInput = null;
+            if(txtMandatory.Text!= string.Empty)
+                strMandatoryInput = txtMandatory.Text.Split(',');
+
+            double[] mandatoryInput = null;
+            if (strMandatoryInput !=null && strMandatoryInput.Length > 0)
+                mandatoryInput = Array.ConvertAll(strMandatoryInput, element => double.Parse(element));
+
+            _minSum = (double)nudMinSum.Value;
+
+            LogMessage("Starting to work on " + _input.Length + " numbers. Sum = " + _input.Sum());
+
+            //int numOfIterations = Convert.ToInt32(Math.Floor(_input.Sum() / 75));
+
+            List<double> lstInput = _input.ToList();
+
+            //_packs = new List<List<Pack>>();
+            _packs = new List<PackSet>();
+
+            GetAllPacks(lstInput, new List<Pack>());
+
+            _packs = ProcessMandatoryNumbers(mandatoryInput);
+
+            //_packs = _packs.OrderByDescending(elm => elm.Sum(item => item.Sum)).ToList();
+            _packs = _packs.Distinct().ToList();
+
+            if (rbSum.Checked)
+            {
+                _packs = _packs.OrderByDescending(elm => elm.Sum).ToList();
+            }
+            else
+            {
+                _packs = _packs.OrderBy(elm => ((elm.Packs.Count * 75) % elm.Sum)).ToList();
+            }
+
+            StartMessage();
+
+            for(int i = 0; i < _packs.Count; i++)
+            {
+                AppendMessage($"******** OPTION {i + 1} ********");
+
+                AppendMessage("Sum: " + _packs[i].Sum);
+
+                _packs[i].Packs.ForEach(elm => AppendMessage("Total: " + elm.Sum + " => " + string.Join(", ", elm.Sums)));
+
+                AppendMessage("**************************");
+            }
+            EndMessage();
+        }
+
+        private List<PackSet> ProcessMandatoryNumbers(double[] mandatoryInput)
+        {
+            if (mandatoryInput == null || mandatoryInput.Length <= 0)
+                return _packs;
+
+            List<PackSet> lstPacks = new List<PackSet>();
+
+            foreach(PackSet packSet in _packs)
+            {
+                if (PackSetHasAllMandatory(packSet, mandatoryInput))
+                    lstPacks.Add(packSet);
+            }
+
+            return lstPacks;
+        }
+
+        private bool PackSetHasAllMandatory(PackSet packSet, double[] mandatoryInput)
+        {
+            foreach(double sum in mandatoryInput)
+            {
+                bool found = false;
+
+                foreach(Pack pack in packSet.Packs)
+                {
+                    if (pack.Sums.Contains(sum))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                    return false;
+                else
+                    found = false;
+            }
+
+            return true;
+        }
+
+        private void GetAllPacks(List<double> lstInput, List<Pack> lstPacks)
+        {
+            List<Pack> result = GetAllCombos(lstInput);
+
+            result = result.Where(elm => (elm.Sum <= 75 && elm.Sum >= _minSum)).Distinct().OrderByDescending(elm => elm.Sum).ToList();
+
+            foreach(Pack pack in result)
+            {
+                //lstPacks.Add(new List<Pack>());
+                //lstPacks.Last().Add(pack);
+
+                List<Pack> lstNewPacks = new List<Pack>(lstPacks);
+
+                lstNewPacks.Add(pack);
+
+                List<double> lstInputTrimmed = new List<double>(lstInput);
+                foreach(double sum in pack.Sums)
+                    lstInputTrimmed.Remove(sum);
+
+                GetAllPacks(lstInputTrimmed, lstNewPacks);
+            }
+
+            if(result.Count == 0)
+            {
+                _packs.Add(new PackSet { Packs = lstPacks });
+            }
+
+            //result.ForEach(elm => LogMessage("Total: " + elm.Sum + " => " + string.Join(", ", elm.Sums)));
+            //lstPacks.ForEach(elm => LogMessage("Total: " + elm[0].Sum + " => " + string.Join(", ", elm[0].Sums)));
+        }
+
+        private List<Pack> GetAllCombos(List<double> list)
+        {
+            int comboCount = (int)Math.Pow(2, list.Count) - 1;
+            //List<List<T>> result = new List<List<T>>();
+            List<Pack> result = new List<Pack>();
+            for (int i = 1; i < comboCount + 1; i++)
+            {
+                // make each combo here
+                result.Add(new Pack());
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if ((i >> j) % 2 != 0)
+                        result.Last().Sums.Add(list[j]);
+                }
+            }
+            return result;
+        }
+
+        private void ClearLog()
+        {
+            txtLog.Clear();
+        }
+
+        private void LogMessage(string message)
+        {
+            txtLog.Text += Environment.NewLine + DateTime.Now + " " + message;
+        }
+
+        private void LogError(string message)
+        {
+            LogMessage("Error: " + message);
+        }
+
+        private void StartMessage()
+        {
+            _logSB = new StringBuilder();
+        }
+
+        private void AppendMessage(string message)
+        {
+            if (_logSB == null)
+                StartMessage();
+
+            _logSB.Append(Environment.NewLine + message);
+        }
+
+        private void EndMessage()
+        {
+            if (_logSB != null)
+                txtLog.Text += _logSB.ToString();
+
+            _logSB = null;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            List<PackSet> lstPackSets = new List<PackSet>();
+
+            Pack p1 = new Pack();
+            p1.Sums.Add(32.95);
+            p1.Sums.Add(24);
+            p1.Sums.Add(10);
+            p1.Sums.Add(8);
+
+            Pack p2 = new Pack();
+            p1.Sums.Add(28.77);
+            p1.Sums.Add(16);
+            p1.Sums.Add(15);
+            p1.Sums.Add(15);
+
+            Pack p3 = new Pack();
+            p1.Sums.Add(55);
+            p1.Sums.Add(19.5);
+
+            PackSet ps1 = new PackSet();
+            ps1.Packs.Add(p1);
+            ps1.Packs.Add(p2);
+            ps1.Packs.Add(p3);
+
+            PackSet ps2 = new PackSet();
+            ps2.Packs.Add(p1);
+            ps2.Packs.Add(p3);
+            ps2.Packs.Add(p2);
+
+            lstPackSets.Add(ps1);
+            lstPackSets.Add(ps2);
+
+            MessageBox.Show(lstPackSets.Count.ToString());
+
+            lstPackSets = lstPackSets.Distinct().ToList();
+
+            MessageBox.Show(lstPackSets.Count.ToString());
+        }
+    }
+
+    class Pack : IEquatable<Pack>
+    {
+        public double Sum
+        {
+            get
+            {
+                return Sums.Sum();
+            }
+        }
+
+
+        public List<int> Numbers { get; set; }
+
+        public List<double> Sums { get; set; }
+
+        public Pack()
+        {
+            //Sum = 0;
+            Numbers = new List<int>();
+            Sums = new List<double>();
+        }
+
+        public override bool Equals(object other)
+        {
+            return Equals(other as Pack);
+        }
+
+        public bool Equals(Pack other)
+        {
+            if (other == null)
+                return false;
+
+            if (other.Sums.Count != this.Sums.Count)
+                return false;
+
+            for(int i = 0; i < other.Sums.Count; i++)
+            {
+                if (other.Sums[i] != this.Sums[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashcode = Sum.GetHashCode();
+
+            foreach (double sum in Sums)
+                hashcode = hashcode ^ sum.GetHashCode();
+
+            return hashcode;
+        }
+    }
+
+    class PackSet : IEquatable<PackSet>
+    {
+        public List<Pack> Packs { get; set; }
+
+        public double Sum
+        {
+            get
+            {
+                return Packs.Sum(elm => elm.Sum);
+            }
+        }
+
+        public PackSet()
+        {
+            Packs = new List<Pack>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as PackSet);
+        }
+
+        public bool Equals(PackSet other)
+        {
+            if (other == null)
+                return false;
+
+            if (other.Packs.Count != this.Packs.Count)
+                return false;
+
+            if (other.Sum != this.Sum)
+                return false;
+
+            foreach(Pack thisPack in this.Packs)
+            {
+                if (!FindEqualPack(thisPack, other))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool FindEqualPack(Pack thisPack, PackSet otherSet)
+        {
+            foreach(Pack otherPack in otherSet.Packs)
+            {
+                if (ArePacksEqual(thisPack, otherPack))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool ArePacksEqual(Pack thisPack, Pack otherPack)
+        {
+            if (thisPack.Sums.Count != otherPack.Sums.Count)
+                return false;
+
+            for(int i = 0; i < thisPack.Sums.Count; i++)
+            {
+                if (thisPack.Sums[i] != otherPack.Sums[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashcode = Sum.GetHashCode();
+
+            foreach(Pack pack in Packs)
+            {
+                foreach (double sum in pack.Sums)
+                    hashcode = hashcode ^ sum.GetHashCode();
+            }
+
+            return hashcode;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static List<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+    }
+
+}
